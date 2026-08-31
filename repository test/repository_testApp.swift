4c4353f1e469d62518c0edc2d0434cfafc6d3705
eleosgosal <eleos_gosal@s2026.ssts.edu.sg>
2026-08-31 16:09:02 +0800

    Initial Commit

---
+++ repository test/repository_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  repository_testApp.swift
+//  repository test
+//
+//  Created by Eleos Gosal on 31/8/26.
+//
+
+import SwiftUI
+
+@main
+struct repository_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
